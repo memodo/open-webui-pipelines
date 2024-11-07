dcf1d13168cea41c616d71c6af9979d2e47b711f
pablo oliva <p.oliva@memodo.de>
2024-11-07 14:51:57 +0100

    Leave only one top-level Pipeline class.

--- examples/pipelines/rag/memodo_rag_pipeline.py
+++ examples/pipelines/rag/memodo_rag_pipeline.py
@@ -23,107 +23,106 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from operator import itemgetter
 
-def printWithEmphasis(toBePrinted):
-    pass
-    # print("**********************************************")
-    # print(f"{toBePrinted}")
-    # print("**********************************************")
-
-class Retreive:
-    def __init__(self, model, host, port, collection_name, openai_api_key):
-        printWithEmphasis(f"Using model: {model}")
-
-        if model.startswith("gpt"):
-            self.model_instance = ChatOpenAI(api_key=SecretStr(openai_api_key), model=model)
-            embeddings = OpenAIEmbeddings()
-        else:
-            self.model_instance = Ollama(model=model)
-            embeddings = OllamaEmbeddings(model=model)
-
-        client = chromadb.HttpClient(host, port)
-        self.vector_db_documents = client.get_collection(
-                name=collection_name
-            )
-
-        logging.basicConfig()
-        logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
-        
-    def lineListOutputParser(self, text: str) -> List[str]:
-        lines = text.strip().split("\n")
-        printWithEmphasis(f"Multi-query questions: {lines}")
-        return lines  
-
-    def query_db(self, questions):
-        results = self.vector_db_documents.query(
-            query_texts=questions,
-            n_results=10
-        )
-        printWithEmphasis(f"Raw Results: {results}")
-        printWithEmphasis(f"Result Documents: {results['documents']}")
-        return results["documents"]
-
-    def preview_results(self, results):
-        printWithEmphasis(f"Preview Results: {results}")
-        return results
-    
-    def do_rag(self, user_question):
-        multiquery_template = """
-        You are an AI language model assistant.
-
-        Your task is to generate 3 different versions of the given user question to retrieve relevant documents from a vector database.
-
-        By generating multiple perspectives on the user question, your goal is to help the user overcome some of the limitations of distance-based similarity search.
-
-        Provide these alternative questions separated by newlines.
-
-        Original question: {question}
-        """
-        
-        rag_template = """
-        Answer the question based on the context below. If you can't answer the question, say "I don't know." Do not make up new information.
-
-        Context: {context}
-
-        Question: {question}
-        """
-
-        multiquery_prompt = PromptTemplate.from_template(multiquery_template)
-        rag_prompt = PromptTemplate.from_template(rag_template)
-
-        parser = StrOutputParser()
-
-        chain = (
-            {
-                "question": lambda x: itemgetter(x["question"])
-            }
-            | multiquery_prompt
-            | self.model_instance
-            | self.lineListOutputParser
-            | {
-                "context": lambda x: self.query_db(x),
-                "question": lambda _: user_question
-            }
-            | rag_prompt
-            | self.model_instance
-            | {
-                "content": lambda x: self.preview_results(x)
-            }
-            | parser
-        )
-
-        try:
-            printWithEmphasis(f"User's question: {user_question}")
-            result = chain.invoke({"question": user_question})
-            printWithEmphasis(f"Result: {result}")
-        except Exception as e:
-            printWithEmphasis(e)
-
 class Pipeline:
     class Valves(BaseModel):
         VECTOR_DB_HOST: str
         VECTOR_DB_PORT: str
         COLLECTION_NAME: str
         MODEL_NAME: str
+
+    class Retreive:
+        def __init__(self, model, host, port, collection_name, openai_api_key):
+            self.printWithEmphasis(f"Using model: {model}")
+
+            if model.startswith("gpt"):
+                self.model_instance = ChatOpenAI(api_key=SecretStr(openai_api_key), model=model)
+                embeddings = OpenAIEmbeddings()
+            else:
+                self.model_instance = Ollama(model=model)
+                embeddings = OllamaEmbeddings(model=model)
+
+            client = chromadb.HttpClient(host, port)
+            self.vector_db_documents = client.get_collection(
+                    name=collection_name
+                )
+
+            logging.basicConfig()
+            logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
+            
+        def printWithEmphasis(toBePrinted):
+            print("**********************************************")
+            print(f"{toBePrinted}")
+            print("**********************************************")
+        
+        def lineListOutputParser(self, text: str) -> List[str]:
+            lines = text.strip().split("\n")
+            self.printWithEmphasis(f"Multi-query questions: {lines}")
+            return lines  
+
+        def query_db(self, questions):
+            results = self.vector_db_documents.query(
+                query_texts=questions,
+                n_results=10
+            )
+            self.printWithEmphasis(f"Raw Results: {results}")
+            self.printWithEmphasis(f"Result Documents: {results['documents']}")
+            return results["documents"]
+
+        def preview_results(self, results):
+            self.printWithEmphasis(f"Preview Results: {results}")
+            return results
+        
+        def do_rag(self, user_question):
+            multiquery_template = """
+            You are an AI language model assistant.
+
+            Your task is to generate 3 different versions of the given user question to retrieve relevant documents from a vector database.
+
+            By generating multiple perspectives on the user question, your goal is to help the user overcome some of the limitations of distance-based similarity search.
+
+            Provide these alternative questions separated by newlines.
+
+            Original question: {question}
+            """
+            
+            rag_template = """
+            Answer the question based on the context below. If you can't answer the question, say "I don't know." Do not make up new information.
+
+            Context: {context}
+
+            Question: {question}
+            """
+
+            multiquery_prompt = PromptTemplate.from_template(multiquery_template)
+            rag_prompt = PromptTemplate.from_template(rag_template)
+
+            parser = StrOutputParser()
+
+            chain = (
+                {
+                    "question": lambda x: itemgetter(x["question"])
+                }
+                | multiquery_prompt
+                | self.model_instance
+                | self.lineListOutputParser
+                | {
+                    "context": lambda x: self.query_db(x),
+                    "question": lambda _: user_question
+                }
+                | rag_prompt
+                | self.model_instance
+                | {
+                    "content": lambda x: self.preview_results(x)
+                }
+                | parser
+            )
+
+            try:
+                self.printWithEmphasis(f"User's question: {user_question}")
+                result = chain.invoke({"question": user_question})
+                self.printWithEmphasis(f"Result: {result}")
+            except Exception as e:
+                self.printWithEmphasis(e)
 
     def __init__(self):
         self.retreive = None
@@ -141,7 +140,7 @@ class Pipeline:
     async def on_startup(self):
         # This function is called when the server is started.
         print(f"on_startup:{__name__}")
-        self.retreive = Retreive(self.valves.MODEL_NAME, self.valves.VECTOR_DB_HOST, self.valves.VECTOR_DB_PORT, self.valves.COLLECTION_NAME, self.valves.OPENAI_API_KEY)
+        self.retreive = self.Retreive(self.valves.MODEL_NAME, self.valves.VECTOR_DB_HOST, self.valves.VECTOR_DB_PORT, self.valves.COLLECTION_NAME, self.valves.OPENAI_API_KEY)
         
 
     async def on_shutdown(self):
